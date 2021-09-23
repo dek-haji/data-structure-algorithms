e2896cd36fdd833f2b34ababa0685456ae4e4cb4
dek-haji <deeeqhaji@gmail.com>
2021-09-23 19:21:23 -0400

    update of the day

---
+++ src/com/company/Files/FileCreator.java
@@ -0,0 +1,5 @@
+package com.company.Files;
+
+public class FileCreator {
+    
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,22 @@ public class Main {
 
     public static void main(String args[])
     {
-        BubbleSort ob = new BubbleSort();
-        int arr[] = {5, 4, 2, 1, 9, 6};
-        ob.bubbleSort(arr);
-        System.out.println("Buble Sorted array");
-        ob.printArray(arr);
+//        BubbleSort ob = new BubbleSort();
+//        int arr[] = {5, 4, 2, 1, 9, 6};
+//        ob.bubbleSort(arr);
+//        System.out.println("Buble Sorted array");
+//        ob.printArray(arr);
+//
+//
+//        //Selection sort class output
+//        selectionSort ob1 = new selectionSort();
+//        int arr2[] = {12,14,16,18,19};
+//        ob1.sort(arr2);
+//        System.out.println("selectionSorted array");
+//        ob1.printArray(arr2);
 
 
-        //Selection sort class output
-        selectionSort ob1 = new selectionSort();
-        int arr2[] = {12,14,16,18,19};
-        ob1.sort(arr2);
-        System.out.println("selectionSorted array");
-        ob1.printArray(arr2);
+
 
     }
 }
