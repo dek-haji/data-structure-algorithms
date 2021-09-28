4de6ced4e8491d5825eea655a825fbe5222c5904
dek-haji <deeeqhaji@gmail.com>
2021-09-28 19:25:56 -0400

    created a graph class

--- src/com/company/Files/Graphs/graph.java
+++ src/com/company/Files/Graphs/graph.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class graph {
     // A utility function to add an edge in an
     // undirected graph
+    //adding edge method
     public void AddEdge(ArrayList<ArrayList<Integer>> adj, int u, int v)
     {
         adj.get(u).add(v);
         adj.get(v).add(u);
+
     }
 }
